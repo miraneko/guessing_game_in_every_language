7a822b0952748dca5f255b00354741bb63067e5d
Mira Ciel Nekomimi-Sireneva <miranekomimisireneva@gmail.com>
2024-12-02 02:00:16 +0400

    Pascal: created

---
+++ Pascal/guessing_game.pas
@@ -0,0 +1,31 @@
+
+Program GuessingGame;
+
+Type GameRange = 1 .. 100;
+
+Var 
+  n : GameRange;
+  guess : GameRange;
+  nStep : Integer;
+Begin
+  Randomize;
+
+  n := Random(100) + 1;
+  nStep := 0;
+
+  WriteLn('You should guess the number in range 1 .. 100');
+
+  Repeat
+    Write('Please enter an integer: ');
+    ReadLn(guess);
+
+    If guess < n Then
+      WriteLn('Your guess is to low :(')
+    Else If guess > n Then
+           WriteLn('Your guess is to high :(');
+
+    nStep := nStep + 1
+  Until (guess = n);
+
+  WriteLn('You won in ', nStep, ' steps!')
+End.
