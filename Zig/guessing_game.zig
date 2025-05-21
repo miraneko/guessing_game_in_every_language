fc5ae218fef5edc670647d2efe910800ea6363b5
Mira Ciel Nekomimi-Sireneva <miranekomimisireneva@gmail.com>
2025-05-21 10:37:24 +0400

    Zig: some refactoring

--- Zig/guessing_game.zig
+++ Zig/guessing_game.zig
@@ -1,5 +1,4 @@
 const std = @import("std");
-const endian = @import("builtin").target.cpu.arch.endian();
 
 pub fn main() !void {
     var prng = std.rand.DefaultPrng.init(blk: {
@@ -7,11 +6,12 @@ pub fn main() !void {
         try std.posix.getrandom(std.mem.asBytes(&seed));
         break :blk seed;
     });
+    const rand = prng.random();
 
     const stdout = std.io.getStdOut().writer();
     const stdin = std.io.getStdIn().reader();
 
-    const n = prng.random().intRangeAtMost(i32, 1, 100);
+    const n = rand.intRangeAtMost(i32, 1, 100);
     var step: i32 = 1;
 
     try stdout.writeAll("You should guess the number in range 1 .. 100\n");
@@ -19,7 +19,17 @@ pub fn main() !void {
     var buffer: [20]u8 = undefined;
     while (true) {
         try stdout.writeAll("Enter your guess: ");
-        const result = try stdin.readUntilDelimiterOrEof(&buffer, '\n') orelse return error.ReadError;
+        const result = stdin.readUntilDelimiterOrEof(&buffer, '\n') catch |err| switch (err) {
+            error.StreamTooLong => {
+                try stdin.skipUntilDelimiterOrEof('\n');
+                try stdout.writeAll("You entered bullshit\n");
+                continue;
+            },
+            else => return err,
+        } orelse {
+            try stdout.writeAll("\nGame cancelled\n");
+            return;
+        };
         const guess = std.fmt.parseInt(i32, result, 10) catch {
             try stdout.writeAll("You entered bullshit\n");
             continue;
